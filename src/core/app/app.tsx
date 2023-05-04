dd67dafa3511779f7920532064df040ebd2178d8
Ariadna Frias <ariadna.812@gmail.com>
2023-05-04 19:10:01 +0200

    Add app tsx file

--- src/core/app/app.tsx
+++ src/core/app/app.tsx
@@ -1,5 +1,11 @@
+import { Footer } from "../components/footer/footer";
+
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default App;
