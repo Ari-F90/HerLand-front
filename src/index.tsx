2e16aa3371be70e653bd1b280fa7a522d7280616
Ariadna Frias <ariadna.812@gmail.com>
2023-05-19 18:43:29 +0200

    Update changes in index tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,20 @@ import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./core/app/app";
 import "./index.scss";
+import { Provider } from "react-redux";
+import { store } from "./core/store/store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const container = document.querySelector(".container")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
