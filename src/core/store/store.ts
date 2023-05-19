6c71473042e30e4ac68219818ace4289a35faa78
Ariadna Frias <ariadna.812@gmail.com>
2023-05-19 18:56:06 +0200

    Moved store ts file

--- src/core/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { figureReducer } from "../../figures/reducer/figures.reducer";
-
-export const store = configureStore({
-  reducer: { figures: figureReducer },
-});
-
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
