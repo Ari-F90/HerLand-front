73081670dd77897a2069e9b5a21c0e1fcdc321aa
Ariadna Frias <ariadna.812@gmail.com>
2023-05-20 20:31:52 +0200

    Update header file and test

--- src/core/components/header/header.test.tsx
+++ src/core/components/header/header.test.tsx
@@ -1,12 +1,25 @@
 import { render, screen } from "@testing-library/react";
 import { Header } from "./header";
+import { MemoryRouter as Router } from "react-router-dom";
+import { store } from "../../../store/store";
+import { Provider } from "react-redux";
 
 describe("Given the Header component", () => {
   describe("When it is rendered", () => {
     test("Then it should appear two images in the screen", () => {
-      render(<Header></Header>);
+      render(
+        <Provider store={store}>
+          <Router>
+            <Header>
+              <></>
+            </Header>
+            ;
+          </Router>
+        </Provider>
+      );
+
       const elements = screen.getAllByRole("img");
-      expect(elements.length).toBe(2);
+      expect(elements.length).toBe(1);
     });
   });
 });
--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -1,22 +1,20 @@
+import { Link } from "react-router-dom";
 import styles from "./header.module.scss";
 
-export function Header() {
+type HeaderProps = { children: JSX.Element };
+export function Header({ children }: HeaderProps) {
   return (
     <div className={styles.header}>
       <div>
-        <img
-          className={styles.header__logo}
-          src="../../../favicon.svg"
-          alt="Favicon logo"
-        ></img>
+        <Link to="/home">
+          <img
+            className={styles.header__logo}
+            src="../../../favicon.svg"
+            alt="Favicon logo"
+          ></img>
+        </Link>
       </div>
-      <div>
-        <img
-          className={styles.header__burger}
-          src="../../../img/menu-burger.png"
-          alt="Burger menu"
-        ></img>
-      </div>
+      <div>{children}</div>
     </div>
   );
 }
