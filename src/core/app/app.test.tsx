49463d9581902423d12140ab8b01424647d123f2
Ariadna Frias <ariadna.812@gmail.com>
2023-05-20 20:31:30 +0200

    Update app test file

--- src/core/app/app.test.tsx
+++ src/core/app/app.test.tsx
@@ -2,11 +2,25 @@ import React from "react";
 import { render } from "@testing-library/react";
 
 import App from "./app";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import { Header } from "../components/header/header";
+import { Footer } from "../components/footer/footer";
+
+jest.mock("../components/header/header");
+
+jest.mock("../components/footer/footer");
 
 describe("Given the app component", () => {
   describe("When it is rendered", () => {
     test("renders learn react link", () => {
-      render(<App />);
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      );
+      expect(Header).toHaveBeenCalled();
+      expect(Footer).toHaveBeenCalled();
     });
   });
 });
