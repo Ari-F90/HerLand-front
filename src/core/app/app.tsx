27df1e30e7f0587842bdac37135538b7e89d5e6c
Ariadna Frias <ariadna.812@gmail.com>
2023-05-20 20:31:37 +0200

    Update app tsx file

--- src/core/app/app.tsx
+++ src/core/app/app.tsx
@@ -1,11 +1,14 @@
 import { Footer } from "../components/footer/footer";
 import { Header } from "../components/header/header";
+import { Menu, menuOptions } from "../components/menu/menu";
 import { Homepage } from "../components/pages/homepage/homepage";
 
 function App() {
   return (
     <div className="container">
-      <Header></Header>
+      <Header>
+        <Menu props={menuOptions}></Menu>
+      </Header>
       <Homepage></Homepage>
       <Footer></Footer>
     </div>
