ec868248f2f6df648dba9b11d78ef6e2957b75bf
Ariadna Frias <ariadna.812@gmail.com>
2023-05-21 22:31:21 +0200

    Add approuter test and file

---
+++ src/core/components/app.router/app.router.test.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable testing-library/no-wait-for-side-effects */
+
+import { MemoryRouter as Router } from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import { AppRouter } from "./app.router";
+import "@testing-library/jest-dom";
+import { Provider } from "react-redux";
+import { store } from "../../../store/store";
+
+describe("Given AppRouter", () => {
+  const renderAppRouter = (number: number) => {
+    render(
+      <Provider store={store}>
+        <Router initialEntries={["/home", "/figures"]} initialIndex={number}>
+          <AppRouter></AppRouter>
+        </Router>
+      </Provider>
+    );
+  };
+  describe("When rendering and the path is '/home'", () => {
+    test("Then it should be titles in the screen", async () => {
+      await waitFor(async () => renderAppRouter(0));
+      const element = await screen.findByRole("heading");
+      expect(element).toBeInTheDocument();
+    });
+  });
+
+  describe("When rendering and the path is '/figures'", () => {
+    test("Then, the title 'About us' should be in the screen", async () => {
+      await waitFor(async () => renderAppRouter(1));
+      const element = await screen.findByRole("heading", {
+        name: "Herstory makers",
+      });
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/core/components/app.router/app.router.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import { menuOptions } from "../menu/menu";
+
+const HomePage = lazy(() => import("../pages/homepage/homepage"));
+const FigureListPage = lazy(
+  () => import("../pages/figureListpage/figureListpage")
+);
+
+export function AppRouter() {
+  return (
+    <Suspense>
+      <Routes>
+        <Route path={"/"} element={<HomePage></HomePage>}></Route>
+        <Route
+          path={menuOptions[0].path}
+          element={<HomePage></HomePage>}
+        ></Route>
+
+        <Route
+          path={menuOptions[2].path}
+          element={<FigureListPage></FigureListPage>}
+        ></Route>
+      </Routes>
+    </Suspense>
+  );
+}
