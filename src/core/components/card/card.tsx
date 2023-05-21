790002edd06923fdc0e8d6a88a0460e3fe7caf5e
Ariadna Frias <ariadna.812@gmail.com>
2023-05-21 22:31:32 +0200

    Add card scss ad test files

---
+++ src/core/components/card/card.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { Figure } from "../../../figures/models/figure";
+import styles from "./card.module.scss";
+
+type CardProps = {
+  figure: Figure;
+};
+
+export function Card({ figure }: CardProps) {
+  return (
+    <div>
+      <li className={styles.card}>
+        <div className={styles.card__element}>
+          <Link to={`/details/${figure.id}`} relative="path">
+            <img
+              className={styles.card__img}
+              src={figure.img}
+              alt="figure's icon"
+            ></img>
+          </Link>
+        </div>
+        <div className={styles.card__properties}>
+          <div className={styles.card__properties__name}>
+            <span>{figure.name}</span>
+          </div>
+        </div>
+      </li>
+    </div>
+  );
+}
