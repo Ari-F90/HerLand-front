4843ea9980d306f6619ae4abdeacf15ece851db8
Ariadna Frias <ariadna.812@gmail.com>
2023-05-23 17:36:17 +0200

    Update homepage files and test

--- src/core/components/pages/homepage/homepage.test.tsx
+++ src/core/components/pages/homepage/homepage.test.tsx
@@ -3,7 +3,7 @@ import HomePage from "./homepage";
 
 jest.mock("./homepage");
 
-describe("Given the Homepage component", () => {
+describe("Given the HomePage component", () => {
   describe("When it is rendered", () => {
     test('Then it should render "Discover"', () => {
       render(
