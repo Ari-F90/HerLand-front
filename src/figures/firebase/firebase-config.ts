f75fada0a15694e4af5066279b35e4efddd0b8f1
Ariadna Frias <ariadna.812@gmail.com>
2023-05-24 21:02:11 +0200

    Install firebase config and test

---
+++ src/figures/firebase/firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: process.env.FB_APIKEY,
+  authDomain: "herland-75f66.firebaseapp.com",
+  projectId: "herland-75f66",
+  storageBucket: "herland-75f66.appspot.com",
+  messagingSenderId: "28135321088",
+  appId: "1:28135321088:web:ab714c46d562c9ad4872e5",
+};
